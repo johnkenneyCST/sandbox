1894c01f9f22c9c5ad21e67a1d47459fe4ab9e15
JohnKenney <john.kenney@cellsignal.com>
2023-10-05 13:37:34 -0400

    Added Print Outs, prep for workcell training

--- scripts/file_watcher/main.py
+++ scripts/file_watcher/main.py
@@ -1,5 +1,6 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import time
 import sys 
 import os
 
@@ -8,15 +9,29 @@ class MyHandler(FileSystemEventHandler):
         if not event.is_directory:
             print("New file created:", event.src_path)
 
+    def on_deleted(self, event):
+        if not event.is_directory:
+            print(f"File Deleted: {event.src_path}")
+
+    
+
 
 if __name__ == "__main__":
+    print("Started")
     observer = Observer()
     event_handler = MyHandler()
     observer.schedule(event_handler, path='./scripts', recursive=False)
     observer.start()
+    iteration = 1
     try:
         while True:
-            pass
+            print(f"Observing{'.'*iteration}")
+            time.sleep(1.5)
+            os.system("cls")
+            iteration += 1
+            if iteration >= 15:
+                iteration = 1
     except KeyboardInterrupt:
         observer.stop()
+        
     observer.join()
